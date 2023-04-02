040dfc04a1ce98944b13649b5f618c4022652bea
everr <everrwsr@outlook.com>
2023-04-02 17:22:21 +0800

    部署到自己服务器上，对数据库信息的修改

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("没有换行符号的时候：");
+        System.out.println("小明    小明");
+        System.out.println("有Windows换行符号的时候：");
+        System.out.println("小明 \n小明");
+        System.out.println("有Linux换行符号的时候：");
+        System.out.println("小明 \r\n小明");
+
+    }
+}
